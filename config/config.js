0eb03c782e645a3be6c7efc97b24ab97b5a6415c
laura.tombs@gmail.com <laura.tombs@gmail.com>
2016-09-16 11:22:34 +0100

    config changes

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port:   process.env.PORT || 3000,
-  db:     process.env.MONGOLAB_URI || "mongodb://localhost/express-authentication-jwt",
+  db:     process.env.MONGODB_URI || "mongodb://localhost/express-authentication-jwt",
   secret: process.env.SECRET || "secrets and such"
 };
